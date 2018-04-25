b392002b17fb46729748ecd4ca1889e190b73a75
BlackCap <mailhph@gmail.com>
2018-04-25 12:43:32 +0200

    http server

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 import com.sun.net.httpserver.HttpServer; // http server
 import java.sql.*; // sqlite
 
+
 class Main {
   public static void main (String[] args) {
-
+    try {
+      Test.main(args);
+    } catch (Exception e) {}
   }
 }
