d86639a7dd9800fb17b2af2ccc2f6b7926ef0c4b
BlackCap <mailhph@gmail.com>
2018-04-25 12:26:07 +0200

    sqlite

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpServer; // http server
+import java.sql.*; // sqlite
 
 class Main {
   public static void main (String[] args) {
