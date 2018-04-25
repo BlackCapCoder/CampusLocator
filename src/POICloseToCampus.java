448f41c63a61750ac96ade4fbf2bc012e46bbfd9
Richard Bolstad <31256985+bolric17@users.noreply.github.com>
2018-04-25 14:42:16 +0200

    Create POICloseToCampus.java

---
+++ src/POICloseToCampus.java
@@ -0,0 +1,13 @@
+class POICloseToCampus {
+  Enum Category { Clothes, Electronics, Food };
+  
+  String name;
+  String location;
+  Category category;
+
+  POICloseToCampus (String name, String location, Category category) {
+    this.name       = name;
+    this.location   = location;
+    this.category   = category;
+  }
+}
