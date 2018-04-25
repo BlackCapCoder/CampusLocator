fb3e824df70188ea9a96336be03b12eea86a3941
BlackCap <mailhph@gmail.com>
2018-04-25 13:54:07 +0200

    application logic

--- src/Directions.java
+++ src/Directions.java
@@ -1,11 +1,40 @@
+import java.util.ArrayList;
 
 // Represents instructions for traveling from A to B
 class Directions {
-  Campus origin;
-  Campus destination;
+  ArrayList<Campus> stops = new ArrayList<Campus> ();
 
   Directions (Campus origin, Campus destination) {
-    this.origin      = origin;
-    this.destination = destination;
+    stops.add(origin);
+    stops.add(destination);
   }
+  Directions () {}
+
+  // Where to start
+  public Campus getOrigin () {
+    return stops.get(0);
+  }
+
+  // Where to end
+  public Campus getDestination () {
+    return stops.get(stops.size() - 1);
+  }
+
+  // Returns all stops on the travel
+  public ArrayList<Campus> getStops () {
+    return stops;
+  }
+
+  // Add a stop
+  public void AddStop (Campus s) {
+    stops.add(s);
+  }
+
+  // Directions are monoid
+  public void Append (Directions other) {
+    stops.addAll(other.getStops());
+  }
+
+  public static Directions Empty () { return new Directions (); }
+
 }
