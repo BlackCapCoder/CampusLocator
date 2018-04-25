36a5f6c55dd7b9241ab3780e134afb7e881a9fa8
BlackCap <mailhph@gmail.com>
2018-04-25 13:41:47 +0200

    application logic

---
+++ src/Campus.java
@@ -0,0 +1,11 @@
+class Campus {
+  String name;
+  boolean isFavorite;
+  String location;
+
+  Campus (String name, boolean isFavorite, String location) {
+    this.name       = name;
+    this.isFavorite = isFavorite;
+    this.location   = location;
+  }
+}
---
+++ src/Directions.java
@@ -0,0 +1,11 @@
+
+// Represents instructions for traveling from A to B
+class Directions {
+  Campus origin;
+  Campus destination;
+
+  Directions (Campus origin, Campus destination) {
+    this.origin      = origin;
+    this.destination = destination;
+  }
+}
