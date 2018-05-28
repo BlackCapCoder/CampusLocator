a1fa8a91bcdeeb7fbbad32dbf421cb85ecfe32f3
BlackCap <mailhph@gmail.com>
2018-05-28 12:44:58 +0200

    project raport

--- src/Html/HtmlWriter.java
+++ src/Html/HtmlWriter.java
@@ -5,21 +5,11 @@ import java.io.PrintWriter;
 
 
 public class HtmlWriter extends PrintWriter {
-  private long size = 0;
-
   public HtmlWriter (OutputStream os) {
     super(os);
   }
 
-  public long getSize () { return this.size; }
-
   public void tag (Tag t) {
     print(t.toString());
   }
-
-  public void print (String str) {
-    size += str.length();
-    super.print(str);
-  }
-
 }
